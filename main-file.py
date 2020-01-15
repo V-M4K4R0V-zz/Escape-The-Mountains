5865367a2171ced7e775e8c68ac54ded35b15555
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-01-15 13:40:42 +0100

    menu

--- main-file.py
+++ main-file.py
@@ -1,17 +1,33 @@
 import pygame
 import sys
+import pygameMenu
+
 
 pygame.init()
 
-gDisplay = pygame.display.set_mode((600,700))
+#window
+screen_s = (600, 700)
+
+gDisplay = pygame.display.set_mode(screen_s)
 pygame.display.set_caption('Escape The Mountain')
 
-icon = "C:\\Users\\ahmed\\Desktop\\work\\Escape-The-Mountains\\icon.png"
+#start menu
+m_size = (200, 300)
 
-pygame.display.set_icon(icon)
-run = True
+pygameMenu.Menu(gDisplay, m_size, font, title, *args) # -> Menu object
 
-while run:
+#background
+background = "C:\\Users\\ahmed\\Desktop\\work\\Escape-The-Mountains\\backg.jpg"
+B_image = pygame.image.load(background).convert()
+gDisplay.blit(B_image, [0, -180])
+
+
+game_over = False
+
+while not game_over:
+
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             sys.exit()
+

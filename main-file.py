b87e600427796f78d493339129317e57ef22abeb
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-01-18 17:06:19 +0100

    jump-a

--- main-file.py
+++ main-file.py
@@ -15,8 +15,8 @@ pygame.display.set_icon(icon)
 
 #player
 player = pygame.image.load('C:\\Users\\ahmed\\Desktop\\work\\Escape-The-Mountains\\characters\\demon1.png')
-playerX = 300
-playerY = 40
+playerX = 350
+playerY = 401
 
 """
 #player 2
@@ -31,8 +31,9 @@ def player_fu(x,y):
     gDisplay.blit(player, (x, y))
 
 move = 8
-
 run = True
+jump = True
+
 while run:
     #background
     background = "C:\\Users\\ahmed\\Desktop\\work\\Escape-The-Mountains\\backgrounds\\The_Hell.png"
@@ -51,18 +52,26 @@ while run:
         playerX -= move
     if keys[pygame.K_RIGHT] and playerX < 1200:
         playerX += move
+    """
     #MOVING Y;
     if keys[pygame.K_UP] and playerY > 2:
         playerY -= move
     if keys[pygame.K_DOWN] and playerY < 401:
         playerY += move
+    """
+    #jumping 
+    if keys[pygame.K_SPACE]:
+        jump = playerY - 30
+        playerY = jump   
+    elif playerY > 2 and playerY < 401:
+        playerY += 10
+        jump = False
+
 
     player_fu(playerX, playerY)
 
-
     """
         #REC player2
-        
         #moving X;
         if keys[pygame.K_LEFT] and playerX2 > 2:
             playerX2 -= move
